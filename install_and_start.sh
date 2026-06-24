b918ccdcfbb15fb6ad73ceb6a873bcdd4dab4607
Ruud Greven <ruud@rgonline.nl>
2026-06-24 14:09:17 +0200

    Added instructions

--- install_and_start.sh
+++ install_and_start.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
+echo "Install and prepare venv"
 cd ~/arcadetogo-main
 python3 -m venv myvenv
 source myvenv/bin/activate
 pip3 install -r requirements.txt
 
 export DISPLAY=:0
+echo "Killing previous instances"
+killall python3
+killall -9 python3
+
+echo "Start GUI"
 python3 main.py
